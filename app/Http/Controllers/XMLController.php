cac33a0adb058992f5e67f95b344c9adec9030e0
Samuel Serrano Escorcia <samuel.serrano@qenta.app>
2021-05-06 16:14:30 -0500

    Lógica de firma

--- app/Http/Controllers/XMLController.php
+++ app/Http/Controllers/XMLController.php
@@ -7,6 +7,11 @@ use ArchivoController;
 use SimpleXMLElement;
 use App\Modules\moduleXML;
 
+use RobRichards\XMLSecLibs\XMLSecurityDSig;
+use RobRichards\XMLSecLibs\XMLSecurityKey;
+use DOMDocument;
+use DOMXPath;
+
 class XMLController extends Controller
 {
 
@@ -18,18 +23,53 @@ class XMLController extends Controller
         */
         try
         {
-        $fechaingreso="AEIOU"; 
-        $fecharetiro="2020-05-23";
-        $fechaliquidacioninicio="2021-04-01"; 
-        $fechaliquidacionfin="2021-04-30"; 
-        $this->moduleXML = new moduleXML();        
+        //$fechaingreso="AEIOU"; 
+        //$fecharetiro="2020-05-23";
+       // $fechaliquidacioninicio="2021-04-01"; 
+       // $fechaliquidacionfin="2021-04-30"; 
+       // $this->moduleXML = new moduleXML();        
         //$respuesta = $this->moduleXML->SetTiempoLaborado($fechaingreso, $fechaliquidacioninicio,$fechaliquidacionfin,$fecharetiro);
         //$respuesta = $this->moduleXML->SetNumeroSecuencia(1001,"XYZ");
         //$respuesta = $this->moduleXML->getDate('hgmt');
         //$respuesta = $this->moduleXML->getCUNE("N00001",$this->moduleXML->getDate('ymd'),$this->moduleXML->getDate('hgmt'),5000450.25,1000000.75,'700085371','800199436',102,693,1);
-        $respuesta = $this->moduleXML-> ArmarNombreArchivo(2,80044123456 ,1616161616);
+        //$respuesta = $this->moduleXML-> ArmarNombreArchivo(2,80044123456 ,1616161616);
         
-        return $respuesta;
+        
+        $doc = new DOMDocument();
+        $doc->load('xmlsignature/xmlgenerated.xml');
+        //si se tiene el string xml se utiliza
+        //  $doc->loadXml("stringXml");
+        $xpath = new DOMXPath($doc);
+        $nodeset = $xpath->query("/NominaIndividual/ext:UBLExtensions/ext:UBLExtension/ext:ExtensionContent")->item(0);        
+        // Create a new Security object 
+        $objDSig = new XMLSecurityDSig();
+        // Use the c14n exclusive canonicalization
+        $objDSig->setCanonicalMethod(XMLSecurityDSig::EXC_C14N);
+        // Sign using SHA-256
+        
+        $objDSig->addReference(
+            $doc, 
+            XMLSecurityDSig::SHA256, 
+            array('http://www.w3.org/2000/09/xmldsig#enveloped-signature')
+        );
+
+        // Create a new (private) Security key
+        $objKey = new XMLSecurityKey(XMLSecurityKey::RSA_SHA256, array('type'=>'private'));
+        $objKey->passphrase = '123456';
+        // Load the private key
+        $objKey->loadKey('xmlsignature/nominademo.pem', TRUE);
+        // Sign the XML file
+        $objDSig->sign($objKey);
+
+        // Add the associated public key to the signature
+        $objDSig->add509Cert(file_get_contents('xmlsignature/certnomina.cer'));
+
+        // Append the signature to the XML
+        $objDSig->appendSignature($nodeset);
+        // Save the signed XML
+        $doc->save('xmlsignature/signed.xml');
+
+        return "xml firmado";
         }
         catch(\Throwable $error)
         {
